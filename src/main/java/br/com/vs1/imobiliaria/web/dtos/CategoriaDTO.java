7d8a75caeab36acd377fabca9d8d8badd98a0856
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-04 03:48:22 -0300

    feat: categories dto

---
+++ src/main/java/br/com/vs1/imobiliaria/web/dtos/CategoriaDTO.java
@@ -0,0 +1,36 @@
+package br.com.vs1.imobiliaria.web.dtos;
+
+public class CategoriaDTO {
+
+    private String nome;
+
+    private String descricao;
+
+
+    public CategoriaDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoriaDTO{" +
+                "nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
