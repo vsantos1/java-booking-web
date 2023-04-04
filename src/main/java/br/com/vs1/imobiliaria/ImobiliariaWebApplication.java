24483547cbafaec9fe7e70928c5ae14f8368cbea
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-04 03:46:27 -0300

    spring main

---
+++ src/main/java/br/com/vs1/imobiliaria/ImobiliariaWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.vs1.imobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobiliariaWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ImobiliariaWebApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/vs1/imobiliaria/ImobiliariaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vs1.imobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
