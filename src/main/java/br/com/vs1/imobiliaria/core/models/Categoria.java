c44821c0f1c740105d7527af79eca1ac0a58f8fd
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-05 03:22:48 -0300

    feat: categories dto and html base

---
+++ src/main/java/br/com/vs1/imobiliaria/core/models/Categoria.java
@@ -0,0 +1,50 @@
+package br.com.vs1.imobiliaria.core.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria  implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
