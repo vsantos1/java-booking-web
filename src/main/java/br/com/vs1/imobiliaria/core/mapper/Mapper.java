507e94c2c4c3a090f15afb82fd6084496833ef56
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-06 03:51:32 -0300

    chore: removed unused imports

--- src/main/java/br/com/vs1/imobiliaria/core/mapper/Mapper.java
+++ src/main/java/br/com/vs1/imobiliaria/core/mapper/Mapper.java
@@ -1,9 +1,6 @@
 package br.com.vs1.imobiliaria.core.mapper;
 
-import br.com.vs1.imobiliaria.web.mapper.WebImovelMapper;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.spi.MatchingStrategy;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
