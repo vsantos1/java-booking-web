bc6109d78de7d0b7c27619ed78ab2d1116cd8903
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-06 03:45:11 -0300

    chore: small changes

--- src/main/java/br/com/vs1/imobiliaria/ImobiliariaWebApplication.java
+++ src/main/java/br/com/vs1/imobiliaria/ImobiliariaWebApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ImobiliariaWebApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ImobiliariaWebApplication.class, args);
-  }
+    public static void main(String[] args) {
+
+        SpringApplication.run(ImobiliariaWebApplication.class, args);
+    }
 
 }
