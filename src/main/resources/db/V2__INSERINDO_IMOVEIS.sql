25c6ddc1f66135847da3a32779a09df5dbd385ae
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-08 04:18:24 -0300

    feat: added mock data

--- src/main/resources/db/V2__INSERINDO_IMOVEIS.sql
+++ src/main/resources/db/V2__INSERINDO_IMOVEIS.sql
@@ -18,3 +18,16 @@ VALUES('2023-04-06', 'Casa com 3 quartos, 2 banheiros, 1 vaga de garagem, 100m²
 INSERT INTO public.tb_imoveis
 (data_cadastro, descricao, disponivel, foto, garagem, nome, preco_aluguel, preco_compra, quantidade_banheiros, quantidade_quartos, tamanho, endereco_id)
 VALUES('2023-04-06', 'Casa com 3 quartos, 2 banheiros, 1 vaga de garagem, 100m² de área construída e 200m² de área total.', true, 'https://picsum.photos/200/300', 3, 'Casa boa vista 21', 57858.00, 500.00, 3, 2, 200, 5);
+
+INSERT INTO public.tb_imoveis
+(data_cadastro, descricao, disponivel, foto, garagem, nome, preco_aluguel, preco_compra, quantidade_banheiros, quantidade_quartos, tamanho, endereco_id)
+VALUES('2023-04-12', 'Casa com 3 quartos, 2 banheiros, 1 vaga de garagem, 100m² de área construída e 200m² de área total.', true, 'https://picsum.photos/200/300', 3, 'Casa boa vista 21', 57858.00, 500.00, 3, 2, 200, 5);
+
+INSERT INTO public.tb_imoveis
+(data_cadastro, descricao, disponivel, foto, garagem, nome, preco_aluguel, preco_compra, quantidade_banheiros, quantidade_quartos, tamanho, endereco_id)
+VALUES('2023-02-12', 'Casa com 3 quartos, 2 banheiros, 1 vaga de garagem, 100m² de área construída e 200m² de área total.', true, 'https://picsum.photos/200/300', 3, 'Casa boa vista 21', 57858.00, 500.00, 3, 2, 200, 5);
+
+INSERT INTO public.tb_imoveis
+(data_cadastro, descricao, disponivel, foto, garagem, nome, preco_aluguel, preco_compra, quantidade_banheiros, quantidade_quartos, tamanho, endereco_id)
+VALUES('2023-04-20', 'Casa com 3 quartos, 2 banheiros, 1 vaga de garagem, 100m² de área construída e 200m² de área total.', true, 'https://picsum.photos/200/300', 3, 'Casa boa vista 21', 57858.00, 500.00, 3, 2, 200, 5);
+
